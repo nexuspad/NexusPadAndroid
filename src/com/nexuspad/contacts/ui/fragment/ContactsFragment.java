305af68d9f4ab26a431acd7b225ea63cf8c06596
echui <echui@ualberta.ca>
2013-08-02 11:21:47 -0600

    wip, switching branch

--- src/com/nexuspad/contacts/ui/fragment/ContactsFragment.java
+++ src/com/nexuspad/contacts/ui/fragment/ContactsFragment.java
@@ -8,11 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
-import com.edmondapps.utils.android.view.ViewUtils;
+import android.widget.SectionIndexer;
 import com.edmondapps.utils.java.WrapperList;
-import com.mobsandgeeks.adapters.Sectionizer;
-import com.mobsandgeeks.adapters.SimpleSectionAdapter;
+import com.emilsjolander.components.stickylistheaders.StickyListHeadersAdapter;
 import com.nexuspad.R;
 import com.nexuspad.annotation.ModuleId;
 import com.nexuspad.datamodel.EntryList;
@@ -20,6 +18,7 @@ import com.nexuspad.datamodel.EntryTemplate;
 import com.nexuspad.datamodel.Folder;
 import com.nexuspad.datamodel.Person;
 import com.nexuspad.dataservice.ServiceConstants;
+import com.nexuspad.ui.EntriesAdapter;
 import com.nexuspad.ui.fragment.EntriesFragment;
 
 import java.util.Collections;
@@ -46,16 +45,26 @@ public class ContactsFragment extends EntriesFragment {
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.list_content_sticky, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getListView().setFastScrollEnabled(true);
+    }
+
+    @Override
     protected void onListLoaded(EntryList list) {
         super.onListLoaded(list);
 
         mContacts = new WrapperList<Person>(list.getEntries());
-        Collections.sort(mContacts);
+        Collections.sort(mContacts, Person.ORDERING_BY_LAST_NAME);
         final BaseAdapter adapter = getListAdapter();
         if (adapter == null) {
             final ContactsAdapter contactsAdapter = new ContactsAdapter();
-            setListAdapter(new SimpleSectionAdapter<Person>(getActivity(), contactsAdapter,
-                    android.R.layout.simple_list_item_1, android.R.id.text1, new PersonSectionizer()));
+            setListAdapter(contactsAdapter);
         } else {
             adapter.notifyDataSetChanged();
         }
@@ -66,58 +75,59 @@ public class ContactsFragment extends EntriesFragment {
         throw new UnsupportedOperationException();
     }
 
-    private class PersonSectionizer implements Sectionizer<Person> {
-        @Override
-        public String getSectionTitleForItem(Person person) {
-            final String lastName = person.getLastName();
-            if (TextUtils.isEmpty(lastName)) {
-                return getString(R.string.others);
-            }
-            if (lastName.length() > 1) {
-                return lastName.substring(0, 1); // first letter
-            }
-            return lastName;
-        }
-    }
-
-    private static class ViewHolder {
-        TextView name;
-    }
-
-    private class ContactsAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return mContacts.size();
+    private class ContactsAdapter extends EntriesAdapter<Person> implements StickyListHeadersAdapter {
+        public ContactsAdapter() {
+            super(getActivity(), mContacts);
         }
 
         @Override
-        public Person getItem(int position) {
-            return mContacts.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final ViewHolder holder;
+        protected View getEntryView(Person p, int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = LayoutInflater.from(getActivity()).inflate(android.R.layout.simple_list_item_1, parent, false);
-
-                holder = new ViewHolder();
-                holder.name = ViewUtils.findView(convertView, android.R.id.text1);
-
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
+                convertView = getLayoutInflater().inflate(R.layout.list_item_icon, parent, false);
             }
+            final ViewHolder holder = getHolder(convertView);
 
-            final Person p = getItem(position);
-            holder.name.setText(getString(R.string.formatted_person_name, p.getLastName(), p.getFirstName(), p.getMiddleName()));
+            holder.text1.setText(getString(R.string.formatted_person_name, p.getLastName(), p.getFirstName(), p.getMiddleName()));
 
             return convertView;
+        }
+
+        @Override
+        protected View getEmptyEntryView(LayoutInflater i, View c, ViewGroup p) {
+            return getCaptionView(i, c, p, R.string.empty_contacts, R.drawable.empty_folder);
+        }
+
+        @Override
+        public View getHeaderView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_header, parent, false);
+            }
+            ViewHolder holder = getHolder(convertView);
+
+            final String lastName = getItem(position).getLastName();
+            if (!TextUtils.isEmpty(lastName) && lastName.length() > 1) {
+                holder.text1.setText(lastName.substring(0, 1));
+            }
+
+            return convertView;
+        }
+
+        @Override
+        protected int getEntryStringId() {
+            return 0;
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            return getHeaderChar(position);
+        }
+
+        private char getHeaderChar(int position) {
+            final String lastName = getItem(position).getLastName();
+            if (!TextUtils.isEmpty(lastName) && lastName.length() > 1) {
+                return lastName.substring(0, 1).charAt(0);
+            }
+            return 0;
         }
     }
 }
