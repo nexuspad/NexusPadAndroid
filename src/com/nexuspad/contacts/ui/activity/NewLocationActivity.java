a5befc18751e0d8251c13ad77fadcacf76fc6f63
Edmond <user.email>
2013-09-08 20:03:48 -0600

    removed InteiJ annotation dep.; UI for viewing & editing Location (address)

---
+++ src/com/nexuspad/contacts/ui/activity/NewLocationActivity.java
@@ -0,0 +1,7 @@
+package com.nexuspad.contacts.ui.activity;
+
+/**
+ * Author: edmond
+ */
+public class NewLocationActivity {
+}
---
+++ src/com/nexuspad/contacts/ui/fragment/NewLocationFragment.java
@@ -0,0 +1,7 @@
+package com.nexuspad.contacts.ui.fragment;
+
+/**
+ * Author: edmond
+ */
+public class NewLocationFragment {
+}
